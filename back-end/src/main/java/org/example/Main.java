343a4ad4ed089017f86b5be39add4d4884cf21e3
Jiahui <forestlin333@gmail.com>
2023-12-01 16:50:23 +0100

    Add ENV for dockerfile

--- back-end/src/main/java/org/example/Main.java
+++ back-end/src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import org.example.models.User;
-import org.example.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
